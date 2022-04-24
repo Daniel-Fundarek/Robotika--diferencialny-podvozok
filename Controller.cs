7682ad7258d023ad53dd771991184953d363e488
Daniel-Fundarek <79155394+Daniel-Fundarek@users.noreply.github.com>
2022-04-24 20:41:16 +0200

    working

---
+++ Controller.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Controller
+    {
+        private double calculateLinVel(double leftWheelVel, double RightWheelVel)
+        {
+
+            return (leftWheelVel + RightWheelVel) / 2;
+
+        }
+
+        private double calculateRotVel(double leftWheelVel, double rightWheelVel, double wheelBase)
+        {
+            return (rightWheelVel - leftWheelVel) / wheelBase;
+        }
+
+        private double calculateAngle(double rotVelocity, double previousAngle, double timeDeviation)
+        {
+            double angle = previousAngle + rotVelocity * timeDeviation;
+            if (angle > 2 * Math.PI)
+            {
+                angle -= 2 * Math.PI;
+            }
+            else if (angle < 0)
+            {
+                angle += 2 * Math.PI;
+            }
+
+            return angle;
+        }
+
+        private void calculatePosition(Position _position, Position _prevPosition, double _linVel, double angleR, double timeDeviation)
+        {
+            _prevPosition.setX(_position.getX());
+            _prevPosition.setY(_position.getY());
+            double linDeviation = _linVel * timeDeviation;
+            _position.setX(_position.getX() + 3 * linDeviation * Math.Sin(angleR)); // *3
+            _position.setY(_position.getY() + 3 * linDeviation * Math.Cos(angleR)); // *3
+
+
+        }
+
+        private void changeVelOfWheel()
+        {
+            if (timeStamps[timeIndex] <= currTime)
+            {
+                velLeftWheel = velLeftWheelArray[timeIndex];
+                velRightWheel = velRightWheelArray[timeIndex];
+                if (timeIndex != timeStamps.Length - 1)
+                {
+                    timeIndex++;
+                }
+
+
+            }
+        }
+    }
+}
